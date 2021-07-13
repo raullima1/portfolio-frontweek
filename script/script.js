53341ebf529da306e997038fd7d07d57c9df3872
Raul Lima <raul.llima1@gmail.com>
2021-07-12 22:43:21 -0300

    First commit

---
+++ script/script.js
@@ -0,0 +1,32 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//   var tl = gsap.timeline();
+
+//   tl
+//   .from(".menu-1", {
+//     duration: 1,
+//     y: -50,
+//     opacity: 0,
+//     delay: 0.5
+//   })
+//   .from(".menu-2", {
+//     duration: 1,
+//     y: -50,
+//     opacity: 0,
+//   }, "-=0.8")
+//   .from(".menu-3", {
+//     duration: 1,
+//     y: -50,
+//     opacity: 0,
+//   }, "-=0.8")
+//   .from(".menu-4", {
+//     duration: 1,
+//     y: -50,
+//     opacity: 0,
+//   }, "-=0.8")
+//   .from(".menu-5", {
+//     duration: 1,
+//     y: -50,
+//     opacity: 0,
+//   }, "-=0.8")
+  
+// })
